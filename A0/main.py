ade740f9232255cb4a698e9e900620951afe268c
Lingyi_Xu <lingyi.xu129@gmail.com>
2024-09-11 18:41:00 -0500

    First commit

---
+++ A0/main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃F5 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi {name}, this is for the Assignment 0! Have a good day!')  # Press F9 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('COMP 576')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
